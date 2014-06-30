4b2cea4cc45d916fe4f5c7cea4bbac7b25f8df1a
sala <saladinkzn@gmail.com>
2014-06-30 23:00:35 +0400

    AnnotationRowMapperFactory refactoring

--- src/main/java/ru/shadam/ftlmapper/mapper/annotation/AnnotationRowMapperFactory.java
+++ src/main/java/ru/shadam/ftlmapper/mapper/annotation/AnnotationRowMapperFactory.java
@@ -21,8 +21,74 @@ import java.util.List;
  */
 public class AnnotationRowMapperFactory {
     public <T> AnnotationRowMapper<T> annotationRowMapper(String prefix, Class<T> clazz) {
-        final Constructor<?>[] constructors = clazz.getConstructors();
+        NewInstanceSupplier<T> creator = getNewInstanceCreator(clazz);
+        //
+        final List<ResultSetConsumer<T>> consumers = new ArrayList<>();
+        //
+        final List<Field> fieldList = collectFields(clazz);
+        for(final Field field: fieldList) {
+            consumers.add(getResultSetConsumer(prefix, clazz, field));
+        }
+        return new AnnotationRowMapper<>(creator, consumers);
+    }
+
+    private <T> List<Field> collectFields(Class<T> clazz) {
+        final List<Field> fieldList = new ArrayList<>();
+        for(Class<? super T> ancestor = clazz; ancestor != null; ancestor = ancestor.getSuperclass()) {
+            if(ancestor.isAnnotationPresent(MappedType.class)) {
+                final Field[] fields = ancestor.getDeclaredFields();
+                Collections.addAll(fieldList, fields);
+            }
+        }
+        return fieldList;
+    }
+
+    private <T> ResultSetConsumer<T> getResultSetConsumer(String prefix, Class<T> clazz, final Field field) {
+        final Property property = field.getAnnotation(Property.class);
+        final Embedded embedded = field.getAnnotation(Embedded.class);
+        if((property == null && embedded == null) || (property != null && embedded != null)) {
+            throw new IllegalArgumentException("Field cannot have nor Property or Embedded annotation neither both");
+        }
+        final ResultSetConsumer<T> resultSetConsumer;
+        if(property != null) {
+            field.setAccessible(true);
+            final String annotatedName = property.value();
+            final String propertyName = prefix + (annotatedName.isEmpty() ? field.getName() : annotatedName);
+            resultSetConsumer = new ResultSetConsumer<T>() {
+                @Override
+                public void consume(T instance, ResultSetWrapper resultSetRow) throws SQLException {
+                    try {
+                        field.set(instance, resultSetRow.getObject(propertyName));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("should not happen");
+                    }
+                }
+            };
+        } else {
+            field.setAccessible(true);
+            final String embeddedPrefix = prefix + embedded.value();
+            final Class<?> embeddedClass = field.getType();
+            if(!clazz.isAnnotationPresent(MappedType.class)) {
+                throw new IllegalStateException("Field mapped with @Embedded should be of class mapped with @MappedType");
+            }
+            final AnnotationRowMapper rowMapper2 = annotationRowMapper(embeddedPrefix, embeddedClass);
+            resultSetConsumer = new ResultSetConsumer<T>() {
+                @Override
+                public void consume(T instance, ResultSetWrapper resultSetRow) throws SQLException {
+                    try {
+                        field.set(instance, rowMapper2.mapRow(resultSetRow));
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("should not happen");
+                    }
+                }
+            };
+        }
+        return resultSetConsumer;
+    }
+
+    private <T> NewInstanceSupplier<T> getNewInstanceCreator(Class<T> clazz) {
         NewInstanceSupplier<T> creator = null;
+        final Constructor<?>[] constructors = clazz.getConstructors();
         for(final Constructor<?> constructor: constructors) {
             if(constructor.isAnnotationPresent(Creator.class)) {
                 if(creator != null) {
@@ -86,54 +152,6 @@ public class AnnotationRowMapperFactory {
                 throw new IllegalArgumentException("If class has no consturctor mapped by @Creator");
             }
         }
-        //
-        final List<ResultSetConsumer<T>> consumers = new ArrayList<>();
-        //
-        final List<Field> fieldList = new ArrayList<>();
-        for(Class<? super T> ancestor = clazz; ancestor != null; ancestor = ancestor.getSuperclass()) {
-            if(ancestor.isAnnotationPresent(MappedType.class)) {
-                final Field[] fields = ancestor.getDeclaredFields();
-                Collections.addAll(fieldList, fields);
-            }
-        }
-        for(final Field field: fieldList) {
-            final Property property = field.getAnnotation(Property.class);
-            if(property != null) {
-                field.setAccessible(true);
-                final String annotatedName = property.value();
-                final String propertyName = prefix + (annotatedName.isEmpty() ? field.getName() : annotatedName);
-                consumers.add(new ResultSetConsumer<T>() {
-                    @Override
-                    public void consume(T instance, ResultSetWrapper resultSetRow) throws SQLException {
-                        try {
-                            field.set(instance, resultSetRow.getObject(propertyName));
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException("should not happen");
-                        }
-                    }
-                });
-            }
-            final Embedded embedded = field.getAnnotation(Embedded.class);
-            if(embedded != null) {
-                field.setAccessible(true);
-                final String embeddedPrefix = prefix + embedded.value();
-                final Class<?> embeddedClass = field.getType();
-                if(!clazz.isAnnotationPresent(MappedType.class)) {
-                    throw new IllegalStateException("Field mapped with @Embedded should be of class mapped with @MappedType");
-                }
-                final AnnotationRowMapper rowMapper2 = annotationRowMapper(embeddedPrefix, embeddedClass);
-                consumers.add(new ResultSetConsumer<T>() {
-                    @Override
-                    public void consume(T instance, ResultSetWrapper resultSetRow) throws SQLException {
-                        try {
-                            field.set(instance, rowMapper2.mapRow(resultSetRow));
-                        } catch (IllegalAccessException e) {
-                            throw new RuntimeException("should not happen");
-                        }
-                    }
-                });
-            }
-        }
-        return new AnnotationRowMapper<>(creator, consumers);
+        return creator;
     }
 }
