8bd762f5aface2ea58fdb7059442cd7a520f70ad
sala <saladinkzn@gmail.com>
2014-07-04 23:23:46 +0400

    resultSetExtractorFactory refactored

--- src/main/java/ru/shadam/ftlmapper/query/ResultSetExtractorFactory.java
+++ src/main/java/ru/shadam/ftlmapper/query/ResultSetExtractorFactory.java
@@ -25,70 +25,54 @@ public class ResultSetExtractorFactory {
 
     public ResultSetExtractor getResultSetExtractor(Method method) {
         final Mapper mapper = method.getAnnotation(Mapper.class);
+        final RowMapper<?> explicitMapper;
+        try {
+            explicitMapper = mapper == null ? null : mapper.value().newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new IllegalArgumentException("If method is annotated with @Mapper annotation this class should be instantiable with empty constructor");
+        }
         //
         final Class<?> methodReturnType = method.getReturnType();
-        final RowMapper<?> rowMapper;
-        if(mapper != null) {
-            try {
-                rowMapper = mapper.value().newInstance();
-                // TODO
+        final Type classToMap;
+        if(methodReturnType.equals(List.class) || methodReturnType.equals(Set.class) || methodReturnType.equals(Map.class)) {
+            final Type genericReturnType = method.getGenericReturnType();
+            if(genericReturnType instanceof ParameterizedType) {
+                final ParameterizedType parameterizedType = (ParameterizedType) genericReturnType;
+                final Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+                classToMap = actualTypeArguments[0];
                 if(methodReturnType.equals(List.class)) {
-                    return new ListResultSetExtractor<>(rowMapper);
-                } else if (methodReturnType.equals(Set.class)) {
-                    return new SetResultSetExtractor<>(rowMapper);
+                    return new ListResultSetExtractor<>(coalesce(explicitMapper, classToMap));
                 } else if (methodReturnType.equals(Map.class)) {
-                    final ParameterizedType genericReturnType = (ParameterizedType) method.getGenericReturnType();
-                    if(!method.isAnnotationPresent(KeyExtractor.class)) {
-                        throw new IllegalArgumentException("Method which returns Map should be annotated with @KeyExtractor");
-                    }
                     final KeyExtractor keyExtractor = method.getAnnotation(KeyExtractor.class);
-                    final String keyExtractorValue = keyExtractor.value();
-                    final Type keyType = genericReturnType.getActualTypeArguments()[0];
-                    final RowMapper keyMapper = rowMapperFactory.getRowMapper(keyType, keyExtractorValue);
-                    return new MapResultSetExtractor<>(keyMapper, rowMapper);
-                } else if (methodReturnType.isArray()) {
-                    return new ArrayResultSetExtractor(rowMapper, methodReturnType);
-                } else {
-                    return new UniqueResultExtractor<>(rowMapper);
-                }
-            } catch (InstantiationException | IllegalAccessException e) {
-                throw new IllegalArgumentException("If method is annotated with @Mapper annotation this class should be instantiable with empty constructor");
-            }
-        } else {
-            final Type classToMap;
-            if(methodReturnType.equals(List.class) || methodReturnType.equals(Set.class) || methodReturnType.equals(Map.class)) {
-                final Type genericReturnType = method.getGenericReturnType();
-                if(genericReturnType instanceof ParameterizedType) {
-                    final ParameterizedType parameterizedType = (ParameterizedType) genericReturnType;
-                    final Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
-                    classToMap = actualTypeArguments[0];
-                    if(methodReturnType.equals(List.class)) {
-                        return new ListResultSetExtractor<>(rowMapperFactory.getRowMapper(classToMap));
-                    } else if (methodReturnType.equals(Map.class)) {
-                        final KeyExtractor keyExtractor = method.getAnnotation(KeyExtractor.class);
-                        if(keyExtractor == null) {
-                            throw new IllegalArgumentException("Map-returning method should be annotated with @KeyExtractor");
-                        }
-                        final RowMapper<?> keyMapper = rowMapperFactory.getRowMapper(classToMap, keyExtractor.value());
-                        final RowMapper<?> valueMapper = rowMapperFactory.getRowMapper(actualTypeArguments[1]);
-                        return new MapResultSetExtractor<>(keyMapper, valueMapper);
-                    } else if (methodReturnType.equals(Set.class)) {
-                        return new SetResultSetExtractor<>(rowMapperFactory.getRowMapper(classToMap));
-                    } else {
-                        throw new IllegalArgumentException("Unknown method return type");
+                    if(keyExtractor == null) {
+                        throw new IllegalArgumentException("Map-returning method should be annotated with @KeyExtractor");
                     }
+                    final RowMapper<?> keyMapper = rowMapperFactory.getRowMapper(classToMap, keyExtractor.value());
+                    final RowMapper<?> valueMapper = coalesce(explicitMapper, actualTypeArguments[1]);
+                    return new MapResultSetExtractor<>(keyMapper, valueMapper);
+                } else if (methodReturnType.equals(Set.class)) {
+                    return new SetResultSetExtractor<>(coalesce(explicitMapper, classToMap));
                 } else {
-                    throw new IllegalArgumentException("unexpected generic return type: " + genericReturnType);
+                    throw new IllegalArgumentException("Unknown method return type");
                 }
-            } else if(methodReturnType.isAssignableFrom(Object[].class)) {
-                return new UniqueResultExtractor<>(rowMapperFactory.getRowMapper(methodReturnType));
-            } else if(methodReturnType.isArray()) {
-                classToMap = methodReturnType.getComponentType();
-                return new ArrayResultSetExtractor(rowMapperFactory.getRowMapper(classToMap), ((Class<?>) classToMap));
             } else {
-                classToMap = methodReturnType;
-                return new UniqueResultExtractor<>(rowMapperFactory.getRowMapper(classToMap));
+                throw new IllegalArgumentException("unexpected generic return type: " + genericReturnType);
             }
+        } else if(methodReturnType.isAssignableFrom(Object[].class)) {
+            return new UniqueResultExtractor<>(coalesce(explicitMapper, methodReturnType));
+        } else if(methodReturnType.isArray()) {
+            classToMap = methodReturnType.getComponentType();
+            return new ArrayResultSetExtractor(coalesce(explicitMapper, classToMap), ((Class<?>) classToMap));
+        } else {
+            classToMap = methodReturnType;
+            return new UniqueResultExtractor<>(coalesce(explicitMapper, classToMap));
         }
+    }
+
+    private RowMapper<?> coalesce(RowMapper mapper, Type type) {
+        if(mapper != null) {
+            return mapper;
+        }
+        return rowMapperFactory.getRowMapper(type);
     }
 }
