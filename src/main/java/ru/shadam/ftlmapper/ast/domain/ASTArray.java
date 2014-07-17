7c6609af4d20db44c0b1c1216245ae41674dd2d7
saladinkzn <saladinkzn@gmail.com>
2014-07-17 10:23:34 +0400

    toString was added for ASTArray

--- src/main/java/ru/shadam/ftlmapper/ast/domain/ASTArray.java
+++ src/main/java/ru/shadam/ftlmapper/ast/domain/ASTArray.java
@@ -7,4 +7,12 @@ public class ASTArray extends ASTCollection {
     public ASTArray(String getName, String setName, Class<?> collectionClass, ASTBase innerObject) {
         super(getName, setName, collectionClass, innerObject);
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("ASTArray{");
+        sb.append("innerObject = ").append(innerObject);
+        sb.append('}');
+        return sb.toString();
+    }
 }
